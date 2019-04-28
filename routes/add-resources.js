7eebe59b09feace6412dce4d05b1c6444795801b
TongChang <tong1212chang@gmail.com>
2019-04-28 19:24:52 +0000

    [add] uuid を計算するようにしました。

--- routes/add-resources.js
+++ routes/add-resources.js
@@ -1,15 +1,20 @@
-var express = require('express');
-var router = express.Router();
-var multer  = require('multer');
-var upload = multer({ dest: 'uploads/' });
+const express = require('express');
+const router = express.Router();
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const uuidv4 = require('uuid/v4');
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
+  let generated_uuid = uuidv4();
+
   console.log('request is %O', req);
-  res.send('respond with a resource');
+  console.log('uuid is [' + generated_uuid + ']' );
+
+  res.send('request is ' + JSON.stringify(req.body) + ', uuid is ' + generated_uuid);
 });
 
-router.post('/upload/', upload.single('thumbnail'), (req, res, next) => {
+router.post('/upload-thumbnail/', upload.single('thumbnail'), (req, res, next) => {
   console.log('request is %O', req);
   res.send('upload done.');
 });
