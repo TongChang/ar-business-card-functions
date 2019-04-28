6e0a47238ba26313ec5aa91c32ab99ef386261f2
TongChang <tong1212chang@gmail.com>
2019-04-28 21:57:08 +0000

    [add] functions のひな型を作成しました。

---
+++ functions/index.js
@@ -0,0 +1,133 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
+const multer  = require('multer');
+const upload = multer({ storage: multer.memoryStorage });
+const uuidv4 = require('uuid/v4');
+
+/**
+ * # リソース情報の取得 ( ID )
+ *
+ * リソースの情報を返却します。
+ *
+ * ## レスポンス
+ *
+ * header: {
+ *   status: success or failed,
+ *   errorCode: 0 ( 正常終了 )
+ * },
+ * body: {
+ *   id,
+ *   facebookId,
+ *   instagramId,
+ *   twitterName,
+ *   url,
+ *   lineId
+ * }
+ *
+ */
+app.get('/:id', (req, res, next) => {
+  let id = req.params.id;
+
+  let resource = {
+    id: id,
+    facebookId: 'facebookId',
+    instagramId: 'instagramId',
+    twitterName: 'twitterName',
+    url: 'url',
+    lineId: 'lineId'
+  };
+
+  let response = {
+    header: {
+      status: 'success',
+      errorCode: 0
+    },
+    body: {
+      resource
+    }
+  };
+
+  res.send(response);
+
+});
+
+/**
+ * # リソースの登録
+ *
+ * リソースの初期情報として、もろもろのパーソナルデータを登録しつつ
+ * リソース ID を生成して返します。
+ *
+ * ## レスポンス
+ *
+ * header: {
+ *   status: success or failed,
+ *   errorCode: 0 ( 正常終了 )
+ * },
+ * body: {
+ *   id ( 生成されたリソースの ID )
+ * }
+ *
+ */
+app.post('/', (req, res, next) => {
+  let generated_uuid = uuidv4();
+
+  console.log('request is %O', req);
+
+  // DB に登録して
+  let resource = req.body;
+
+  let response = {
+    header: {
+      status: 'success',
+      errorCode: 0
+    },
+    body: {
+      id: generated_uuid
+    }
+  };
+
+  res.send(JSON.stringify(response));
+});
+
+/**
+ * # ファイルアップロード
+ *
+ * リソースのファイルをアップロードします。
+ *
+ * リソース ID をパスに持ち、その ID に紐づくフォルダに格納します。
+ *
+ * ## レスポンス
+ *
+ * header: {
+ *   status: success or failed,
+ *   errorCode: 0 ( 正常終了 )
+ * },
+ * body: {}
+ *
+ */
+app.post('/upload-images/:id', upload.fields([ {name: 'thumbnail'}, {name: 'marker'} ]), (req, res, next) => {
+  console.log('req.params is %O', req.params);
+  console.log('req.files is %O', req.files);
+
+  let id = req.params.id;
+
+  let tmpThumbnail = req.files.thumbnail;
+  let tmpMarker = req.files.marker;
+
+  // フォルダ作って
+  // ファイル置いて
+
+  let response = {
+    header: {
+      status: 'success',
+      errorCode: 0
+    },
+    body: {}
+  };
+  res.send(JSON.stringify(response));
+});
+
+const api = functions.https.onRequest(app);
+module.exports = { api };
+
